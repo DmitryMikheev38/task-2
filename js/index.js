8120319b67cdf7a0bfc2d68c0701fd43f01ce848
Dmitry Mikheev <dima.m1993@mail.ru>
2019-06-11 21:54:31 +0300

    11.06.2019 настройка стилей датапикера

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 
 
 window.onload = function () {
-  $('#111').datepicker({
+  $('.calendar').datepicker({
     range: true,
     toggleSelected: false,
     offset: 0 + 'px'
   });
-  var d =  $('#111').data('datepicker');
+  var d =  $('.calendar').data('datepicker');
 
   var arrival = document.getElementById('arrival');
   var exit = document.getElementById('exit');
@@ -15,6 +15,10 @@ window.onload = function () {
   var dateArrival = document.getElementById('arrival');
   var dateExit = document.getElementById('exit');
   var cells = document.querySelector('.datepicker--cells');
+  var i = document.querySelector('.datepicker--nav-title i');
+
+  console.log(i.tagName)
+  i.style.cssText = "font-family: 'Quicksand', sans-serif; font-size: 19px; font-weight: bold"
   
   function pad(n) {
     if (n < 10)
@@ -37,22 +41,35 @@ window.onload = function () {
   datepicker.onclick = event => {
     minRange = d.minRange || d._focused;
     maxRange = d.maxRange;
-    var minDate = pad(minRange.getDate()) + '.' + pad(minRange.getMonth()) + '.' + minRange.getFullYear();
-    var maxDate = '';
-    if (maxRange !== '') {
+    var minDate;
+    var maxDate;
+    console.log(minRange)
+    if (minRange) {
+      minDate = pad(minRange.getDate()) + '.' + pad(minRange.getMonth()) + '.' + minRange.getFullYear();
+    } else {
+      minDate = '';
+    
+    }
+    if (maxRange) {
       maxDate = pad(maxRange.getDate()) + '.' + pad(maxRange.getMonth()) + '.' + maxRange.getFullYear();
+    } else {
+      maxDate = '';
     }
     dateArrival.setAttribute('value', minDate);
-    dateExit.setAttribute('value', maxDate)
+    dateExit.setAttribute('value', maxDate);
 
     event.cancelBubble = true;    
   }
   
 
-  var calendaButton = document.createElement('button');
-  calendaButton.innerHTML = "подобрать номер"
-  datepicker.appendChild(calendaButton)
-  calendaButton.classList.add("gradient--button");
+  var calendarButtonClear = document.createElement('a');
+  var calendarButtonApply = document.createElement('a');
+  calendarButtonClear.innerHTML = "очистить";
+  calendarButtonApply.innerHTML = "применить";
+  datepicker.appendChild(calendarButtonClear);
+  datepicker.appendChild(calendarButtonApply);
+  calendarButtonClear.className = "montserrat12 uppercase link weight-bold button-datepicker-clear";
+  calendarButtonApply.classList = "montserrat12 uppercase link weight-bold button-datepicker-apply"
 }
 
 
